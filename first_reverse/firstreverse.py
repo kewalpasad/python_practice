d5ab727b87d35d9cd207dfe01825092e357b21b3
Kewal Pasad <Kewal2123@gmail.com>
2019-01-15 09:31:49 -0500

    firstreverse added

---
+++ first_reverse/firstreverse.py
@@ -0,0 +1,9 @@
+def FirstReverse(string): 
+    
+    string = string[::-1]
+    # code goes here 
+    return string
+    
+# keep this function call here  
+print (FirstReverse("Coderbyte"))
+print (FirstReverse("I love code"))
